17e39cf0aa6f5f9eb7497b858198ca2e66b0a637
Keegan <keeganfarkas@gmail.com>
2022-08-26 17:53:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav.jsx'
+import Header from './components/Header.jsx'
+
+function App() {
+  return (
+    <div className='red'>
+      <Nav></Nav>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
